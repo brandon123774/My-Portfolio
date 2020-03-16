864f3825c8db3ea2832ee9a662dd2162b3d997f0
Brandon Shankar <bshankar12@comcast.net>
2020-03-15 23:21:10 -0700

    updates

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -7,7 +7,7 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/portfolio", function(req, res) {
+app.get("portfolio", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/portfolio.html"));
 });
 app.get("/contact", function(req, res) {
